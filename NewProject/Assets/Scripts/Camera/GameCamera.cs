6032d5a22c921e876c21a99ab4867b103ffc2f7b
Sergey <barbedcrow@gmail.com>
2018-10-07 02:21:32 +0300

    grass floor

---
+++ NewProject/Assets/Scripts/Camera/GameCamera.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameCamera : MonoBehaviour
+{
+
+    public void SetPlayer(Transform player)
+    {
+        this.player = player;
+    }
+
+    #region private
+
+    Transform player;
+
+    private void Update()
+    {
+        if (player == null)
+        {
+            return;
+        }
+
+        transform.position = new Vector3(player.position.x, transform.position.y, player.position.z);
+    }
+
+    #endregion
+}
--- NewProject/Assets/Scripts/Utils/Tags.cs
+++ NewProject/Assets/Scripts/Utils/Tags.cs
@@ -7,5 +7,6 @@ public class Tags : MonoBehaviour
 
     public const string BULLET = "Bullet";
     public const string PLAYER = "Player";
+    public const string GAME_CAMERA = "GameCamera";
 
 }
