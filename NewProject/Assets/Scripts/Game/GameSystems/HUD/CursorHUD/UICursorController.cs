68f4abf19d23742934188dd8f75a32984f5dc455
paxan102 <paxan2012@ya.ru>
2018-10-09 15:08:49 +0300

    Add HUDCrosshairHP

--- NewProject/Assets/Scripts/Game/GameSystems/HUD/CursorHUD/UICursorController.cs
+++ NewProject/Assets/Scripts/Game/GameSystems/HUD/CursorHUD/UICursorController.cs
@@ -16,33 +16,53 @@ public class UICursorController : UIBaseController
 
         detector.OnTriggerIn.AddListener(CursorTriggerOn);
         detector.OnTriggerOut.AddListener(CursorTriggerOut);
+
     }
 
     #region private
 
     UICursorViewData data = new UICursorViewData();
     bool isEnemy = false;
+    Enemy enemy;
 
     void CursorTriggerOn(Collider other)
     {
-        if (other.GetComponent<Enemy>() != null)
+        var collider = other.GetComponent<Enemy>();
+        if (collider != null)
         {
             isEnemy = true;
+            enemy = collider;
+            enemy.OnDie.AddListener(EnemyDead);
         }
     }
 
     void CursorTriggerOut(Collider other)
     {
-        if (other.GetComponent<Enemy>() != null)
+        var collider = other.GetComponent<Enemy>();
+        if (collider != null)
         {
             isEnemy = false;
+            enemy.OnDie.RemoveListener(EnemyDead);
+            enemy = null;
         }
+    }
+
+    void EnemyDead(DamageInfo info)
+    {
+        isEnemy = false;
+        enemy = null;
+        enemy.OnDie.RemoveListener(EnemyDead);
     }
 
     void Update()
     {
         data.mousePoition = Input.mousePosition;
         data.isEnemy = isEnemy;
+        if (enemy)
+        {
+            data.currHP = enemy.GetDamagable().GetCurrentHealth();
+            data.maxHP = enemy.GetDamagable().GetMaxHealth();
+        }
         uiView.UpdateUI(data);
     }
 
